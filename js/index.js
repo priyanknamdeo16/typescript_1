078ac00f0794bebb7ea378dc98aab14ed2ffc36b
priyanknamdeo16 <namdeo.priyank@gmail.com>
2017-11-05 18:25:33 +0530

    overloads

--- js/index.js
+++ js/index.js
@@ -1,17 +1,16 @@
-var myHouse = {
-    name: 'kul',
-    rooms: 1,
-    window: 2,
-    getPerson: function () {
-        var _this = this;
-        return function () {
-            return {
-                name: _this.name,
-                age: 30
-            };
-        };
+//ovrloading
+// number -> object
+// object -> number
+function a(x) {
+    if (typeof x === 'number') {
+        return { name: 'priyank' };
     }
-};
-var fn = myHouse.getPerson();
-console.log(fn());
+    if (typeof x === 'object') {
+        return 10;
+    }
+}
+console.log(a({ name: 'priyank' })); //10
+console.log(a(10)); //{name : 'priyank'}
+console.log(a(null)); //10 because typeof null is object
+console.log(a(true)); //WARNING: [ts] Argument of type 'boolean' is not assignable to parameter of type 'number'.
 //# sourceMappingURL=index.js.map
